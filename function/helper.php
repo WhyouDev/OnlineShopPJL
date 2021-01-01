ef07c6efbfc0b4b9abbeb206761c58030e90fc8a
Ikha Wahyu Ramadhan <sarongtech@Ikhas-MacBook-Pro.local>
2021-01-01 13:52:17 +0700

    maintenance all function on the website modified on MacOS catalina

--- function/helper.php
+++ function/helper.php
@@ -1,5 +1,5 @@
 <?php 
-    define("BASE_URL", "http://localhost/OnlineShopPJL/");
+    define("BASE_URL", "http://localhost/Development/OnlineShopPJL/");
 
     function rupiah($nilai = 0){
         $string = "Rp." .number_format($nilai);
